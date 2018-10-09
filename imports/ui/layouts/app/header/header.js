260b3fbd7cd830d76415fd6d420b9ad78a4e4298
Zachary Freeman <zaksfreeman@gmail.com>
2018-10-09 19:12:48 -0400

    Styling

--- imports/ui/layouts/app/header/header.js
+++ imports/ui/layouts/app/header/header.js
@@ -1,1 +1,8 @@
 import './header.html';
+
+// M.AutoInit();
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.tooltipped');
+//   var instances = M.Tooltip.init(elems, options);
+// });
