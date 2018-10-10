dd9041b5c48d640476cc6f94384c2632d3861e88
Zachary Freeman <zaksfreeman@gmail.com>
2018-10-10 11:47:03 -0400

    Update Meteor to 1.8, update Materialize to 1.0.0

--- client/main.js
+++ client/main.js
@@ -1,4 +1,8 @@
 // Client entry point, imports all client code
-
 import '/imports/startup/client';
 import '/imports/startup/both';
+
+import 'materialize-css/dist/js/materialize.min';
+import 'materialize-css/dist/css/materialize.min.css';
+import Materialize from 'materialize-css';
+global.M = global.Materialize = Materialize;
